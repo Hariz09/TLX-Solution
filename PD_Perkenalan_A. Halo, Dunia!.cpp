d94a3fd052ae3e45b57111cc47ef571d39133c32
Hriz <103235970+Hariz09@users.noreply.github.com>
2023-03-27 13:54:41 +0700

    Add files via upload

---
+++ PD_Perkenalan_A. Halo, Dunia!.cpp
@@ -0,0 +1,12 @@
+// PD_Perkenalan_A. Halo, Dunia!
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+
+int main() {
+	fast
+	cout << "Halo, dunia!";
+}
+/*
+*/
